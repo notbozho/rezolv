c0c2732e31c51500b9dea9b901dc15b463deabed
Bozho Ivanov <bozhoivanow@gmail.com>
2024-10-25 13:48:15 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,8 @@ export default function HomePage() {
             <FAQ />
             <AboutUs />
             <QuoteSection
+                title="Securely build and audit your smart contracts with expert precision"
+                description="Our comprehensive Web3 service handles every step—from secure development to meticulous security audits. Get in touch for a quote tailored to your project."
                 gradientDirection="br"
                 gradientColors={
                     "from-neutral-900 via-black via-40% to-neutral-950"
